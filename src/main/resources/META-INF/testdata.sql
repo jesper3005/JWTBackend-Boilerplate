ab940736bc22fb652aac728e4b5ef199e6baa104
unknown <jesper300595@gmail.com>
2019-04-24 22:43:28 +0200

    initial commit

---
+++ src/main/resources/META-INF/testdata.sql
@@ -0,0 +1,7 @@
+USE seed;
+INSERT INTO users (user_name, user_pass) VALUES ('user', '1234');
+INSERT INTO users (user_name, user_pass) VALUES ('admin', 'admin');
+INSERT INTO roles (role_name) VALUES ('user');
+INSERT INTO roles (role_name) VALUES ('admin');
+INSERT INTO user_roles (user_name, role_name) VALUES ('user', 'user');
+INSERT INTO user_roles (user_name, role_name) VALUES ('admin', 'admin');
